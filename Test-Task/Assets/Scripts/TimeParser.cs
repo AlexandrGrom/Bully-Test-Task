fcc18e74f1bce5e52b1a647836353e38c0b99eb5
AlexandrGrom <68745248+AlexandrGrom@users.noreply.github.com>
2023-11-26 20:09:37 +0200

    added watch mesh

--- Test-Task/Assets/Scripts/TimeParser.cs
+++ Test-Task/Assets/Scripts/TimeParser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -8,20 +9,29 @@ public class TimeParser : MonoBehaviour
     [SerializeField] private Transform arrowM;
     [SerializeField] private Transform arrowS;
     [SerializeField] private TextMeshPro week;
+    [SerializeField] private MeshRenderer arrowMesh;
+    private Skin[] skins;
+    private Skin skin;
     
     
-    private void Update()
-    {
-        DateTime gmtTime = DateTime.UtcNow;
-        week.text = ((int)gmtTime.DayOfWeek).ToString();
-
-
-        float rotationAngleXHour = -gmtTime.Hour % 12 * (360f / 12f);
-        float rotationAngleXMinute = -gmtTime.Minute * (360f / 60f);
-        float rotationAngleXSecond = -gmtTime.Second * (360f / 60f);
-
-        arrowH.localRotation = Quaternion.Euler(rotationAngleXHour, 0f, 0f);
-        arrowM.localRotation = Quaternion.Euler(rotationAngleXMinute, 0f, 0f);
-        arrowS.localRotation = Quaternion.Euler(rotationAngleXSecond, 0f, 0f);
+    private IEnumerator Start()
+    { 
+        skins = Resources.LoadAll<Skin>("Skins");
+        Debug.Log(skins.Length);
+        var waitTime = new WaitForSeconds(1);
+        while (true)
+        {
+            DateTime gmtTime = DateTime.UtcNow;
+            week.text = ((int)gmtTime.DayOfWeek).ToString();
+    
+            float rotationAngleXHour = -gmtTime.Hour % 12 * (360f / 12f);
+            float rotationAngleXMinute = -gmtTime.Minute * (360f / 60f);
+            float rotationAngleXSecond = -gmtTime.Second * (360f / 60f);
+    
+            arrowH.localRotation = Quaternion.Euler(rotationAngleXHour, 0f, 0f);
+            arrowM.localRotation = Quaternion.Euler(rotationAngleXMinute, 0f, 0f);
+            arrowS.localRotation = Quaternion.Euler(rotationAngleXSecond, 0f, 0f);
+            yield return waitTime;
+        }
     }
 }
