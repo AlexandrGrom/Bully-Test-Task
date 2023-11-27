996456fe3e4021f87847e73a99d04ba9ca3a70ff
AlexandrGrom <68745248+AlexandrGrom@users.noreply.github.com>
2023-11-27 21:23:49 +0200

    added pooling system

---
+++ Test-Task/Assets/Scripts/MetallicSphere.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MetallicSphere : PoolableObject
+{
+    [SerializeField] private new Rigidbody rigidbody;
+    private Coroutine coroutine;
+    private void Reset()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        rigidbody.AddForce(force, ForceMode.Impulse);
+    }
+
+    public override void Reinitialize()
+    {
+        base.Reinitialize();
+        
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        coroutine = StartCoroutine(TrackPool());
+        
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+    }
+
+}
